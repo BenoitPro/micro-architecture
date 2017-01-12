9aba4122acfa4f67573e017863cbaf78d4e36ca4
BenoitPro <benoitmarechal@mini-de-benoit.home>
2017-01-12 18:46:56 +0100

    bootstrap et structure html

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -1,4 +1,4 @@
-console.log('RENDER 4 !')
+console.log('Main.js')
 
 var AppView = Backbone.View.extend({
     // el - stands for element. Every view has an element associated with HTML content, will be rendered.
@@ -9,9 +9,29 @@ var AppView = Backbone.View.extend({
     },
     // $el - it's a cached jQuery object (el), in which you can use jQuery functions to push content. Like the Hello TutorialsPoint in this case.
     render: function() {
-        console.log('RENDER !')
         this.$el.html("Hello TutorialsPoint!!!");
     }
 });
 
 var appView = new AppView();
+
+
+//'Router' is a name of the router class
+var Router = Backbone.Router.extend({
+
+    //The 'routes' maps URLs with parameters to functions on your router
+    routes: {
+        "myroute": "myFunc"
+    },
+
+    //'The function 'myFunc' defines the links for the route on the browser
+    myFunc: function(myroute) {
+        document.write(myroute);
+    }
+});
+
+//'router' is an instance of the Router
+var router = new Router();
+
+//Start listening to the routes and manages the history for bookmarkable URL's
+Backbone.history.start();
