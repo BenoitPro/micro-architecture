d1b80618bb309f6ff2d8ca9530ce79c6add9e723
BenoitPro <benoitmarechal@mini-de-benoit.home>
2017-01-13 15:46:40 +0100

    articles index and linsting

--- app/server.js
+++ app/server.js
@@ -29,12 +29,27 @@ var articles = [{
 }];
 
 app.get('/articles/', function(req, res) {
-    res.json(articles);
-    //    res.send('Hello world yeah 4\n');
+
+    MongoClient.connect("mongodb://mongo:27017/blogdb", function(err, db) {
+        if (err) {
+            // todo return error 500...
+            res.end("ERROR");
+            return console.dir(err);
+        }
+        var articles = db.collection('articles');
+
+        articles.find().toArray(function(err, items) {
+            res.json(items);
+        });
+        //    res.send('Hello world yeah 4\n');
+        db.close();
+    });
+
 });
 
 app.get('/articles/:id', function(req, res) {
     console.log("GET : /articles/" + req.params.id);
+    res.json(req.params);
     // posts.get(req.params.id, function(err, post, key) {
     //     if (err) {
     //         console.log("Erreur : ", err);
@@ -58,40 +73,40 @@ var MongoClient = require('mongodb').MongoClient;
 // Mongo mais du temps a se lancer et la tentative de connection plante
 // car elle est réalisé avant que mongodb soit lancé...
 // Todo Add waiting code for mongo to be ready...
-MongoClient.connect("mongodb://mongo:27017/blogdb", function(err, db) {
-    if (err) {
-        return console.dir(err);
-    }
-
-    // Get the documents collection
-    var collection = db.collection('articles');
-
-    //Create some users
-    var article1 = {
-        title: "Title 1 from mongo",
-        content: "Content 1"
-    };
-    var article2 = {
-        title: "Title 2 from mongo",
-        content: "Content 2"
-    };
-    var article3 = {
-        title: "Title 3 from mongo",
-        content: "Content 3"
-    };
-
-    // Insert some users
-    collection.insert([article1, article2, article3], function(err, result) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('I Inserted %d documents into the "articles" collection. The documents inserted with "_id" are:', result.result.n, result);
-        }
-        // Close connection
-        db.close();
-    });
-
-});
+// MongoClient.connect("mongodb://mongo:27017/blogdb", function(err, db) {
+//     if (err) {
+//         return console.dir(err);
+//     }
+//
+//     // Get the documents collection
+//     var collection = db.collection('articles');
+//
+//     //Create some users
+//     var article1 = {
+//         title: "Title 1 from mongo",
+//         content: "Content 1"
+//     };
+//     var article2 = {
+//         title: "Title 2 from mongo",
+//         content: "Content 2"
+//     };
+//     var article3 = {
+//         title: "Title 3 from mongo",
+//         content: "Content 3"
+//     };
+//
+//     // Insert some users
+//     collection.insert([article1, article2, article3], function(err, result) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log('I Inserted %d documents into the "articles" collection. The documents inserted with "_id" are:', result.result.n, result);
+//         }
+//         // Close connection
+//         db.close();
+//     });
+//
+// });
 
 // REDIS TEST
 var redis = require("redis"),
